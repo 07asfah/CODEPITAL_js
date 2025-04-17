0865320c77e99d2977ea6c8dafe14a59d9904373
Hafsa <moussaidhafsa2@gmail.com>
2025-04-17 20:27:09 +0100

    last modification

--- public/js/app.js
+++ public/js/app.js
@@ -1,68 +1,4 @@
 
-/*<CODEPITAL/>
-```
-# Welcome to Codepital:
-
-In this exercise, we have patients who will go to a doctor to get debugged. The doctor will diagnose them and prescribe a remedy. After that, the patients will go to the pharmacy to buy their remedy, take it, and be cured.
-
-## Description of patients:
-# Welcome to Codepital:
-
-In this exercise, we have patients who will go to a doctor to get debugged. The doctor will diagnose them and prescribe a remedy. After that, the patients will go to the pharmacy to buy their remedy, take it, and be cured.
-
-## Description of patients:
-
-Patients have a name, an illness, money, a pocket, a health condition, knowledge of how to go to a place, take medication, and pay. At the beginning, patients are in a waiting room.
-
-| Name      | Illness              | Money | Pocket | Health State | Treatment              | Go To | Take Care | Pay |
-|-----------|----------------------|-------|--------|--------------|------------------------|-------|-----------|-----|
-| Marcus    | Indentation Error    | 100   | Empty  | Ill          |                        |       |           |     |
-| Optimus   | Unsave               | 200   | Empty  | Ill          |                        |       |           |     |
-| Sangoku   | 404                  | 80    | Empty  | Ill          |                        |       |           |     |
-| DarthVader| Asthmatic            | 110   | Empty  | Ill          |                        |       |           |     |
-| Semicolon | Syntax Error         | 60    | Empty  | Ill          |                        |       |           |     |
-
-
-## Description of the doctor:
-
-The doctor receives patients in his office. First, he diagnoses them and gets paid before prescribing a treatment. Note that the doctor always makes the patient leave his office before taking the next one. In his office, there is his Sphynx cat to maintain a sterile environment. The cat meows every two seconds in the console (bonus). The consultation costs 50€. Patients are in a treatment state before leaving the office.
-
-| Name      | Money | Office | Diagnosis | Patient In | Patient Out |
-|-----------|-------|--------|------------|------------|-------------|
-| Debugger  | 0     | [Cat]  |            |            |             |
-
-### Diagnosis Grid:
-
-| Illness            | Treatment           |
-|---------------------|---------------------|
-| Indentation Error  | `Ctrl+Shift+F`      |
-| Unsave             | `SaveOnFocusChange` |
-| 404                | `CheckLinkRelation` |
-| Asthmatic          | `Ventolin`          |
-| Syntax Error       | `F12+Doc`           |
-
-
-
-
-## The Pharmacy:
-
-Patients will then go to the pharmacy and receive their treatment if they have enough money. If they have enough money, they will be in good health; otherwise, they will be dead, and you will need to push them into a cemetery.
-
-### Treatment Rates:
-
-| Treatment           | Price |
-|---------------------|-------|
-| `Ctrl+Shift+F`      | 60€   |
-| `SaveOnFocusChange` | 100€  |
-| `CheckLinkRelation` | 35€   |
-| `Ventolin`          | 40€   |
-| `F12+Doc`           | 20€   |
-
-
-# Verification:
-
-Thanks to your debugger, follow the evolution of each patient. Make sure they leave the waiting room each time before entering the doctor's office, and they should leave the office before letting someone else enter.*/
-
 class patient {
     constructor(name, Illness, money, healthState) {
         this.name = name;
@@ -71,8 +7,6 @@ class patient {
         this.pocket = [];
         this.healthState = healthState;
         this.treatment = "";
-
-        
     }
 
     goTo(location) {
@@ -98,9 +32,6 @@ class patient {
         } else {
             console.log(`${this.name} doesn't have enough money`);
             console.log(`${this.name} died and has been sent to the cemetry`);
-
-
-            // cemetery.people = cemetery.patient || [];
 
             cemetery.people.push(this)
             this.healthState = "dead"
